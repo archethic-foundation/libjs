579795e268713e1bfba2b46d939c0be543e3a35e
Samuel Manzanera <samuel@uniris.io>
2020-07-02 17:34:56 +0200

    init repository

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+module.exports = {
+    entry: './index.js',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: "Uniris",
+      libraryTarget: "umd"
+    },
+    devtool: 'source-map'
+}
