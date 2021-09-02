52f06eb64300c63dff74da30f3adc4d01246e260
Samuel <samuel@uniris.io>
2021-09-02 18:51:40 +0200

    Support multiple secrets

--- test/transaction_builder.js
+++ test/transaction_builder.js
@@ -24,23 +24,18 @@ describe("Transaction builder", () => {
         })
     })
 
-    describe("setSecret", () => {
-        it("should insert the secret into the transaction data", () => {
-            const tx = new TransactionBuilder("transfer").setSecret("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-            assert.deepStrictEqual(tx.data.keys.secret, hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"))
+    describe("addSecret", () => {
+        it("should add a secret with its authorized keys into the transaction data", () => {
+            const tx = new TransactionBuilder("transfer")
+							.addSecret("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88", {
+								"0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646": "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
+							})
+
+            assert.deepStrictEqual(tx.data.keys.secrets, [hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")])
+            assert.deepStrictEqual(tx.data.keys.authorizedKeys[0][hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646")], hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"))
         })
     })
 
-    describe("addAuthorizedKey", () => {
-        it("should add an authorized key to the transaction data", () => {
-            const publicKey = "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"
-            const encryptedKey = "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-
-            const tx = new TransactionBuilder("transfer").addAuthorizedKey(publicKey, encryptedKey)
-
-            assert.deepStrictEqual(tx.data.keys.authorizedKeys[publicKey], hexToUint8Array(encryptedKey))
-        })
-    })
 
     describe("addUCOTransfer", () => {
         it("should add an uco transfer to the transaction data", () => {
@@ -86,8 +81,9 @@ describe("Transaction builder", () => {
           const secret = "mysecret"
 
             const tx = new TransactionBuilder("transfer")
-                .addAuthorizedKey("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-                .setSecret(secret)
+                .addSecret(secret, { 
+									"0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646": "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
+								})
                 .addUCOTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 0.2020)
                 .addNFTTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 100, "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
                 .setCode(code)
@@ -114,6 +110,8 @@ describe("Transaction builder", () => {
                 //Content size
                 encodeInt32(content.length),
                 new TextEncoder().encode(content),
+	        //Nb of secrets
+		Uint8Array.from([1]),
                 //Secret size
                 encodeInt32(secret.length),
                 new TextEncoder().encode(secret),
@@ -139,7 +137,7 @@ describe("Transaction builder", () => {
     describe("build", () => {
         it("should build the transaction and the related signature", () => {
             const tx = new TransactionBuilder("transfer")
-                .addAuthorizedKey("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
+                .addUCOTransfer("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 10.0)
                 .build("seed", 0, "P256")
 
             assert.deepStrictEqual(tx.address, hexToUint8Array("001680dab710eca8bc6b6c8025e57ebaf2d30c03d8d23a21ba7f8a157c365c5d49"))
@@ -165,8 +163,9 @@ describe("Transaction builder", () => {
       `
 
             const tx = new TransactionBuilder("transfer")
-                .addAuthorizedKey("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-                .setSecret("mysecret")
+                .addSecret(secret, { 
+									"0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646": "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
+								})
                 .addUCOTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 0.2020)
                 .addNFTTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 100, "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
                 .setCode(code)
@@ -189,19 +188,30 @@ describe("Transaction builder", () => {
                 //Content size
                 encodeInt32(content.length),
                 new TextEncoder().encode(content),
+								//Nb secrets
+							  Uint8Array.from([1]),
                 //Secret size
                 encodeInt32(secret.length),
                 new TextEncoder().encode(secret),
                 // Nb of authorized keys
                 Uint8Array.from([1]),
                 // Authorized keys encoding
-                concatUint8Arrays([hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")]),
+                concatUint8Arrays([
+									hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
+								]),
                 // Nb of uco transfers
                 Uint8Array.from([1]),
-                concatUint8Arrays([hexToUint8Array("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), encodeFloat64(0.2020)]),
+                concatUint8Arrays([
+									hexToUint8Array("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), 
+									encodeFloat64(0.2020)
+								]),
                 // Nb of NFT transfers
                 Uint8Array.from([1]),
-                concatUint8Arrays([hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"), hexToUint8Array("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), encodeFloat64(100)]),
+                concatUint8Arrays([
+									hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"), 
+									hexToUint8Array("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"), 
+									encodeFloat64(100)
+								]),
                 // Nb of recipients
                 Uint8Array.from([1]),
                 hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
@@ -232,9 +242,12 @@ describe("Transaction builder", () => {
             const transactionKeyPair = Crypto.deriveKeyPair("seed", 0)
             const nextTransactionKeyPair = Crypto.deriveKeyPair("seed", 1)
 
+
             const tx = new TransactionBuilder("transfer")
-                .addAuthorizedKey("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-                .addUCOTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 0.2193)
+					      .addUCOTransfer("00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", 0.2193)
+                .addSecret(Uint8Array.from([0, 1, 2, 3, 4]), {
+								  "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646": "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
+								})
                 .build("seed", 0)
                 .originSign(originKeypair.privateKey)
 
@@ -248,7 +261,8 @@ describe("Transaction builder", () => {
             assert.strictEqual(parsedTx.previousPublicKey, uint8ArrayToHex(transactionKeyPair.publicKey))
             assert.strictEqual(parsedTx.previousSignature, uint8ArrayToHex(previousSig))
             assert.strictEqual(parsedTx.originSignature, uint8ArrayToHex(originSig))
-            assert.strictEqual(parsedTx.data.keys.authorizedKeys["0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"], "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
+	    assert.strictEqual(parsedTx.data.keys.secrets[0], uint8ArrayToHex(Uint8Array.from([0, 1, 2, 3, 4])))
+            assert.strictEqual(parsedTx.data.keys.authorizedKeys[0]["0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"], "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
             assert.deepStrictEqual(parsedTx.data.ledger.uco.transfers[0], { to: "00b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", amount:  0.2193})
         })
     })
