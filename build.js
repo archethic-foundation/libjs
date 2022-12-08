2db5d6054deaf170dcc1453817dfd29d27b3c325
Bastien CHAMAGNE <bastien@chamagne.fr>
2022-12-08 17:11:04 +0100

    Create a ESM build for node
    It fixes the XMLHttpRequest is not defined error when "import"ing library from node.
    We now have 2 ESM in the dist, one built for node, one build for the browser.
    Quick reminder on why there are 4 files:
    
    browser: archethic.js
    browser import: archethic-browser.mjs
    node import: archethic-node.mjs
    node require: archethic.cjs

--- build.js
+++ build.js
@@ -33,7 +33,22 @@ esbuild.build({
   bundle: true,
   minify: true,
   sourcemap: true,
-  outfile: 'dist/archethic.mjs',
+  outfile: 'dist/archethic-browser.mjs',
   inject: ['./esbuild.inject.js'],
   format: 'esm'
+}).catch(() => process.exit(1))
+
+esbuild.build({
+  logLevel: "info",
+  entryPoints: ['index.js'],
+  bundle: true,
+  minify: true,
+  sourcemap: true,
+  outfile: 'dist/archethic-node.mjs',
+  inject: ['./esbuild.inject.js'],
+  format: 'esm',
+  platform: "node",
+  banner: {
+    js: "import { createRequire } from 'module';const require = createRequire(import.meta.url);"
+  },
 }).catch(() => process.exit(1))
