33a5ec5b43f4cfba3c244afd75a5e1a475c4cfbf
Risson <aime.risson.1@gmail.com>
2023-04-18 17:20:06 +0200

    jest config

---
+++ jest.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+  preset: "ts-jest",
+  resolver: "ts-jest-resolver",
+  transform: {'^.+\\.ts?$': 'ts-jest' },
+  testEnvironment: 'node',
+  testRegex: '/tests/.*\\.(test|spec)?\\.(ts|tsx)$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
