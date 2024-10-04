350048b8bd4b874f8f90cb1d782171f48b092f07
Samuel Manzanera <samuelmanzanera@protonmail.com>
2024-10-04 11:39:08 +0200

    Support object for recipient arg

--- tests/transaction_builder.test.ts
+++ tests/transaction_builder.test.ts
@@ -1,9 +1,7 @@
-import TransactionBuilder from "../src/transaction_builder";
+import TransactionBuilder, { VERSION } from "../src/transaction_builder";
 import { deriveAddress, deriveKeyPair, sign, verify } from "../src/crypto";
 import { concatUint8Arrays, hexToUint8Array, intToUint32Array, intToUint64Array, parseBigInt } from "../src/utils";
 import TE from "../src/typed_encoding";
-
-const VERSION = 3;
 
 // all assert should be transformed to jest expect
 describe("Transaction builder", () => {
@@ -118,16 +116,23 @@ describe("Transaction builder", () => {
 
   describe("addRecipient", () => {
     it("should add a recipient for named action", () => {
-      const tx = new TransactionBuilder("transfer").addRecipient(
-        "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-        "vote",
-        ["Miles"]
-      );
+      const tx = new TransactionBuilder("transfer")
+        .addRecipient(
+          "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
+          "vote",
+          ["Miles"]
+        )
+        .addRecipient("0000ae12908c889ba2728614abc6ae472ed9f1df0b3afefd1faa290473a47ceb42bd", "addHero", { name: "John Doe"})
 
-      expect(tx.data.recipients.length).toBe(1);
+      expect(tx.data.recipients.length).toBe(2);
       expect(tx.data.recipients[0].action).toBe("vote");
-      expect(tx.data.recipients[0].args!.length).toBe(1);
-      expect(tx.data.recipients[0].args![0]).toBe("Miles");
+
+      const listArgs = tx.data.recipients[0].args as string[]
+      expect(listArgs.length).toBe(1);
+      expect(listArgs[0]).toBe("Miles");
+
+      const objectArg = tx.data.recipients[1].args as { name: string }
+      expect(objectArg.name).toBe("John Doe");
     });
 
     it("should throw if types are incorrect", () => {
@@ -173,7 +178,8 @@ describe("Transaction builder", () => {
         )
         .setCode(code)
         .setContent(content)
-        .addRecipient("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88");
+        .addRecipient("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
+        .addRecipient("0000ae12908c889ba2728614abc6ae472ed9f1df0b3afefd1faa290473a47ceb42bd", "addHero", { name: "John Doe"});
 
       const keypair = deriveKeyPair("seed", 0);
 
@@ -231,122 +237,21 @@ describe("Transaction builder", () => {
         // Nb of byte to encode nb of recipients
         Uint8Array.from([1]),
         // Nb of recipients
-        Uint8Array.from([1]),
+        Uint8Array.from([2]),
         // 0 = unnamed recipient
         Uint8Array.from([0]),
-        hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
+        hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
+        // 1 = named recipient
+        Uint8Array.from([1]),
+        hexToUint8Array("0000ae12908c889ba2728614abc6ae472ed9f1df0b3afefd1faa290473a47ceb42bd"),
+        Uint8Array.from([7]),
+        new TextEncoder().encode("addHero"),
+        // Nb args
+        Uint8Array.from([1]),
+        TE.serialize({ name: "John Doe"})
       );
       expect(payload).toEqual(expected_binary);
     });
-
-    // it("should generate binary encoding of the transaction before signing with named action", () => {
-    //   const code = `
-    //           condition inherit: [
-    //             uco_transferred: 0.020
-    //           ]
-
-    //           actions triggered by: transaction do
-    //               set_type transfer
-    //               add_uco_ledger to: "000056E763190B28B4CF9AAF3324CF379F27DE9EF7850209FB59AA002D71BA09788A", amount: 0.020
-    //           end
-    //         `;
-
-    //   const content =
-    //     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet leo egestas, lobortis lectus a, dignissim orci.";
-
-    //   const secret = "mysecret";
-
-    //   const tx = new TransactionBuilder("transfer")
-    //     .addOwnership(secret, [
-    //       {
-    //         publicKey: "0001b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-    //         encryptedSecretKey: "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-    //       }
-    //     ])
-    //     .addUCOTransfer("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", parseBigInt("0.202"))
-    //     .addTokenTransfer(
-    //       "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-    //       parseBigInt("100"),
-    //       "0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-    //     )
-    //     .setCode(code)
-    //     .setContent(content)
-    //     .addRecipient("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88", "vote_for_mayor", [
-    //       "Ms. Smith"
-    //     ]);
-
-    //   const keypair = deriveKeyPair("seed", 0);
-
-    //   tx.address = deriveAddress("seed", 1);
-    //   tx.previousPublicKey = keypair.publicKey;
-
-    //   const payload = tx.previousSignaturePayload();
-
-    //   const expected_binary = concatUint8Arrays(
-    //     //Version
-    //     intToUint32Array(VERSION),
-    //     tx.address,
-    //     Uint8Array.from([253]),
-    //     //Code size
-    //     intToUint32Array(code.length),
-    //     new TextEncoder().encode(code),
-    //     //Content size
-    //     intToUint32Array(content.length),
-    //     new TextEncoder().encode(content),
-    //     // Nb of byte to encode nb of ownerships
-    //     Uint8Array.from([1]),
-    //     //Nb of ownerships
-    //     Uint8Array.from([1]),
-    //     //Secret size
-    //     intToUint32Array(secret.length),
-    //     new TextEncoder().encode(secret),
-    //     // Nb of byte to encode nb of authorized keys
-    //     Uint8Array.from([1]),
-    //     // Nb of authorized keys
-    //     Uint8Array.from([1]),
-    //     // Authorized keys encoding
-    //     concatUint8Arrays(
-    //       hexToUint8Array("0001b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-    //       hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-    //     ),
-    //     // Nb of byte to encode nb of uco transfers
-    //     Uint8Array.from([1]),
-    //     // Nb of uco transfers
-    //     Uint8Array.from([1]),
-    //     concatUint8Arrays(
-    //       hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-    //       intToUint8Array(parseBigInt("0.202"))
-    //     ),
-    //     // Nb of byte to encode nb of Token transfers
-    //     Uint8Array.from([1]),
-    //     // Nb of Token transfers
-    //     Uint8Array.from([1]),
-    //     concatUint8Arrays(
-    //       hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
-    //       hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-    //       intToUint8Array(parseBigInt("100")),
-    //       Uint8Array.from([1]),
-    //       Uint8Array.from([0])
-    //     ),
-    //     // Nb of byte to encode nb of recipients
-    //     Uint8Array.from([1]),
-    //     // Nb of recipients
-    //     Uint8Array.from([1]),
-    //     // 1 = named recipient
-    //     Uint8Array.from([1]),
-    //     hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
-    //     // action
-    //     // action size on 1 byte
-    //     Uint8Array.from([14]),
-    //     // action value
-    //     new TextEncoder().encode("vote_for_mayor"),
-    //     // args size
-    //     Uint8Array.from([1]),
-    //     // args value
-    //     TE.serialize("Ms. Smith")
-    //   );
-    //   expect(payload).toEqual(expected_binary);
-    // });
 
     it("should order the keys or named action args in the generated binary", () => {
       const tx = new TransactionBuilder("transfer").addRecipient(
@@ -469,110 +374,6 @@ describe("Transaction builder", () => {
       expect(verify(tx.previousSignature, tx.previousSignaturePayload(), tx.previousPublicKey)).toBeTruthy();
     });
   });
-
-  // describe("originSignaturePayload", () => {
-  //   it("should generate binary encoding of the transaction before signing", () => {
-  //     const secret = "mysecret";
-  //     const content =
-  //       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sit amet leo egestas, lobortis lectus a, dignissim orci.";
-  //     const code = `condition inherit: [
-  //                           uco_transferred: 0.020
-  //                         ]
-
-  //                         actions triggered by: transaction do
-  //                             set_type transfer
-  //                             add_uco_ledger to: "000056E763190B28B4CF9AAF3324CF379F27DE9EF7850209FB59AA002D71BA09788A", amount: 0.020
-  //                         end
-  //     `;
-
-  //     const tx = new TransactionBuilder("transfer")
-  //       .addOwnership(secret, [
-  //         {
-  //           publicKey: "0001b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-  //           encryptedSecretKey: "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-  //         },
-  //         {
-  //           publicKey: "0001a1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-  //           encryptedSecretKey: "00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-  //         }
-  //       ])
-  //       .addUCOTransfer("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646", parseBigInt("0.202"))
-  //       .addTokenTransfer(
-  //         "0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646",
-  //         parseBigInt("100"),
-  //         "0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"
-  //       )
-  //       .setCode(code)
-  //       .setContent(content)
-  //       .addRecipient("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-  //       .build("seed", 0, "P256");
-
-  //     const transactionKeyPair = deriveKeyPair("seed", 0, Curve.P256);
-  //     const previousSig = sign(tx.previousSignaturePayload(), transactionKeyPair.privateKey);
-
-  //     const payload = tx.originSignaturePayload();
-  //     const expected_binary = concatUint8Arrays(
-  //       //Version
-  //       intToUint32Array(VERSION),
-  //       tx.address,
-  //       Uint8Array.from([253]),
-  //       //Code size
-  //       intToUint32Array(code.length),
-  //       new TextEncoder().encode(code),
-  //       //Content size
-  //       intToUint32Array(content.length),
-  //       new TextEncoder().encode(content),
-  //       // Nb of byte to encode nb of ownerships
-  //       Uint8Array.from([1]),
-  //       //Nb ownerships
-  //       Uint8Array.from([1]),
-  //       //Secret size
-  //       intToUint32Array(secret.length),
-  //       new TextEncoder().encode(secret),
-  //       // Nb of byte to encode nb of authorized key
-  //       Uint8Array.from([1]),
-  //       // Nb of authorized keys
-  //       Uint8Array.from([2]),
-  //       // Authorized keys encoding
-  //       concatUint8Arrays(
-  //         hexToUint8Array("0001a1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-  //         hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
-  //         hexToUint8Array("0001b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-  //         hexToUint8Array("00501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88")
-  //       ),
-  //       // Nb of byte to encode nb of uco transfers
-  //       Uint8Array.from([1]),
-  //       // Nb of uco transfers
-  //       Uint8Array.from([1]),
-  //       concatUint8Arrays(
-  //         hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-  //         intToUint8Array(parseBigInt("0.202"))
-  //       ),
-  //       // Nb of byte to encode nb of Token transfers
-  //       Uint8Array.from([1]),
-  //       // Nb of Token transfers
-  //       Uint8Array.from([1]),
-  //       concatUint8Arrays(
-  //         hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
-  //         hexToUint8Array("0000b1d3750edb9381c96b1a975a55b5b4e4fb37bfab104c10b0b6c9a00433ec4646"),
-  //         intToUint8Array(parseBigInt("100")),
-  //         Uint8Array.from([1]),
-  //         Uint8Array.from([0])
-  //       ),
-  //       // Nb of byte to encode nb of recipients
-  //       Uint8Array.from([1]),
-  //       // Nb of recipients
-  //       Uint8Array.from([1]),
-  //       // 0 = unnamed recipient
-  //       Uint8Array.from([0]),
-  //       hexToUint8Array("0000501fa2db78bcf8ceca129e6139d7e38bf0d61eb905441056b9ebe6f1d1feaf88"),
-  //       transactionKeyPair.publicKey,
-  //       Uint8Array.from([previousSig.length]),
-  //       previousSig
-  //     );
-  //     expect(payload).toStrictEqual(expected_binary);
-  //   });
-  // });
 
   describe("originSign", () => {
     it("should sign the transaction with a origin private key", () => {
